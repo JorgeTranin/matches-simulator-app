e40f9c2c65f1a5eb6b9765b26b0aa781c6e758f9
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2023-04-20 18:24:16 -0400

    Mudança da Main Activity para Java

---
+++ app/src/main/java/com/matches_simulator_app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.matches_simulator_app;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.matches_simulator_app.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity{
+
+    private ActivityMainBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
