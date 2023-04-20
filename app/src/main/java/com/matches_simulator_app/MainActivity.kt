f07f1e45739ee26770298e161d0fe87b9f238295
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2023-04-20 18:08:24 -0400

    Configuração do Binding

--- app/src/main/java/com/matches_simulator_app/MainActivity.kt
+++ app/src/main/java/com/matches_simulator_app/MainActivity.kt
@@ -2,10 +2,16 @@ package com.matches_simulator_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.matches_simulator_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+
     }
 }
